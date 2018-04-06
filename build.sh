a25f8d52169a928b27f28ba47fb3ec0ae0fa8b78
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2018-04-06 17:29:15 -0400

    removed unused font-face in style

--- build.sh
+++ build.sh
@@ -5,8 +5,12 @@ echo "starting to build code-gov font"
 fontello-cli install --config config.json --css css --font font
 
 head README.md -n 8 > NEW_README.md
+
 echo "copy font face command to new README.md"
 sed -n 1,11p css/code-gov.css >> NEW_README.md
+
+sed -i -e '1,11d;' css/code-gov.css
+
 tail README.md >> NEW_README.md
 
 mv NEW_README.md README.md
