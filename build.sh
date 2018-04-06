904238e129637411682d9e412dd8177177974067
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2018-04-06 17:05:39 -0400

    update

--- build.sh
+++ build.sh
@@ -3,3 +3,9 @@ rm -fr css && rm -fr font && rm -fr fontello
 
 echo "starting to build code-gov font"
 fontello-cli install --config config.json --css css --font font
+
+echo "clear README"
+sed -e '9,19d' README.md
+
+echo "copy font face command to README.md"
+sed -n 1,11p css/code-gov.css
