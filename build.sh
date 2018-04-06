f839dd71c41843fce6359f525d308124cfc18748
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2018-04-06 17:49:58 -0400

    added code-gov-font-face.css snippet

--- build.sh
+++ build.sh
@@ -6,10 +6,14 @@ fontello-cli install --config config.json --css css --font font
 
 head README.md -n 8 > NEW_README.md
 
-echo "copy font face command to new README.md"
-sed -n 1,11p css/code-gov.css >> NEW_README.md
+echo "copy font face command to separate file"
+sed -n 1,11p css/code-gov.css > css/code-gov-font-face.css
 
+# remove font face code from code-gov-css
 sed -i -e '1,11d;' css/code-gov.css
+
+# add font-face-code to new README.md
+cat css/code-gov-font-face.css >> NEW_README.md
 
 tail README.md >> NEW_README.md
 
